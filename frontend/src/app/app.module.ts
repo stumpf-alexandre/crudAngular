71af5d1b86f3ec8887f203c41ec285ef37d03e64
stumpf-alexandre <als.stumpf@gmail.com>
2022-03-02 00:09:19 -0300

    criando uma view/home e alterando alguns components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,14 +13,17 @@ import { NavComponent } from './components/template/nav/nav.component';
 
 //importando mais dois modulos para a aplicação
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +31,8 @@ import {MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
