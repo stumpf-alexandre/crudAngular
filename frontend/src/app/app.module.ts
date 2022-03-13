f0875044b678c08ef00d5fd411d2be69f26a0ee9
stumpf-alexandre <als.stumpf@gmail.com>
2022-03-13 18:13:48 -0300

    criado o botão create e implementando o exquematic no read

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,6 +23,16 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +43,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component
   ],
   imports: [
     BrowserModule,
@@ -44,7 +56,14 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,23 +10,30 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: null as any,
+  };
+
   constructor(
     private productService: ProductService, 
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     
   }
 
-  //cria um novo produto e apresenta a mensagem
+  //cria um novo produto e apresenta a mensagem e voltar para a pagina de produto
   createProduct(): void{
-    this.productService.showMessage('Produto criado com sucesso!')
+   
+      this.productService.showMessage('Produto criado com sucesso!');
+      this.router.navigate(['/products']);
+    
   }
 
   //deleta o produto e volta para a pagina de produtos
   cancel(): void{
-    this.router.navigate(['/products'])
+    this.router.navigate(['/products']);
   }
-
 }
